cd773f5f92f2461ad53874984b8e6a518f78ea82
ndkieen <kienngo123max@gmail.com>
2024-06-13 10:07:27 +0700

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'BÀI TẬP LỚN '),
+      home: const MyHomePage(title: 'BÀI TẬP LỚN'),
     );
   }
 }
@@ -41,65 +41,140 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final DatabaseReference _databaseRef = FirebaseDatabase.instance.ref();
+
   void _toggleLed(String ledKey, bool currentState) {
     _databaseRef.child(ledKey).set(!currentState);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BÀI TẬP LỚN'),
-        centerTitle: true,
-      ),
-      body: StreamBuilder(
-        stream: _databaseRef.onValue,
-        builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
-          if (snapshot.hasData && snapshot.data!.snapshot.value != null) {
-            Map<dynamic, dynamic> data =
-                snapshot.data!.snapshot.value as Map<dynamic, dynamic>;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('BÀI TẬP LỚN'),
+          centerTitle: true,
+        ),
+        body: StreamBuilder(
+          stream: _databaseRef.onValue,
+          builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+            if (snapshot.hasData && snapshot.data!.snapshot.value != null) {
+              Map<dynamic, dynamic> data =
+                  snapshot.data!.snapshot.value as Map<dynamic, dynamic>;
 
-            int nhietDo = data['NhietDo'] ?? 0;
-            int nongDoKhiGa = data['NongDoKhiGa'] ?? 0;
+              double nhietDo = data['NhietDo'] ?? 0;
+              int nongDoKhiGa = data['NongDoKhiGa'] ?? 0;
 
-            // Safe parsing of LED states
-            bool led1State = data['Led1'] is bool ? data['Led1'] : false;
-            bool led2State = data['Led2'] is bool ? data['Led2'] : false;
-            bool led3State = data['Led3'] is bool ? data['Led3'] : false;
+              // Safe parsing of LED states
+              bool led1State = data['Led1'] is bool ? data['Led1'] : false;
+              bool led2State = data['Led2'] is bool ? data['Led2'] : false;
+              bool led3State = data['Led3'] is bool ? data['Led3'] : false;
 
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Nhiệt độ: $nhietDo°C',
-                    style: const TextStyle(fontSize: 24),
-                  ),
-                  Text(
-                    'Nồng độ khí ga: $nongDoKhiGa',
-                    style: const TextStyle(fontSize: 24),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => _toggleLed('Led1', led1State),
-                    child: Text(led1State ? 'Tắt LED 1' : 'Bật LED 1'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => _toggleLed('Led2', led2State),
-                    child: Text(led2State ? 'Tắt LED 2' : 'Bật LED 2'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => _toggleLed('Led3', led3State),
-                    child: Text(led3State ? 'Tắt LED 3' : 'Bật LED 3'),
-                  ),
-                ],
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Lỗi: ${snapshot.error}'));
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+              return Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Nhiệt độ: $nhietDo°C',
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                    Slider(
+                      value: nongDoKhiGa.toDouble(),
+                      min: 0,
+                      max: 6000,
+                      divisions: 600,
+                      label: '$nongDoKhiGa',
+                      onChanged: (double value) {},
+                    ),
+                    Expanded(
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        padding: const EdgeInsets.all(16.0),
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                fixedSize: const Size(100, 100),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                backgroundColor:
+                                    led1State ? Colors.red : Colors.green),
+                            onPressed: () => _toggleLed('Led1', led1State),
+                            child: Text(
+                              led1State ? 'OFF' : 'ON',
+                              style: const TextStyle(
+                                fontSize: 28, // Kích thước chữ
+                                fontWeight: FontWeight.bold, // Độ đậm
+                                color: Colors.white, // Màu chữ
+                                // Khoảng cách giữa các chữ
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(100, 100),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              backgroundColor:
+                                  led2State ? Colors.red : Colors.green,
+                            ),
+                            onPressed: () => _toggleLed('Led2', led2State),
+                            child: Text(
+                              led2State ? 'OFF' : 'ON',
+                              style: const TextStyle(
+                                fontSize: 28, // Kích thước chữ
+                                fontWeight: FontWeight.bold, // Độ đậm
+                                color: Colors.white, // Màu chữ
+                                // Khoảng cách giữa các chữ
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(100, 100),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              backgroundColor:
+                                  led3State ? Colors.red : Colors.green,
+                            ),
+                            onPressed: () => _toggleLed('Led3', led3State),
+                            child: Text(
+                              led3State ? 'OFF' : 'ON',
+                              style: const TextStyle(
+                                fontSize: 28, // Kích thước chữ
+                                fontWeight: FontWeight.bold, // Độ đậm
+                                color: Colors.white, // Màu chữ
+                                // Khoảng cách giữa các chữ
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(100, 100),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              backgroundColor: Colors.blueGrey,
+                            ),
+                            onPressed: () {},
+                            child: const Text('Reserved'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Lỗi: ${snapshot.error}'));
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
